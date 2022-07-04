5d80acbcfa9b5307448e2a3f96d7092d374e4567
DxAmbar <d.ambarbaeza@gmail.com>
2022-07-04 12:01:48 -0400

    JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,52 @@
 const cipher = {
-  // ...
+  
+  encode: function (texto, desplazamiento) {
+    let resultado = "";
+    let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    desplazamiento = (desplazamiento % 27 + 27) % 27; // desplazamiento 
+
+    if(texto){
+      for(let i=0; i<texto.length; i++){ //si texto existe, se recorre cadena (letra en array)
+        console.log(texto)
+        if(letras.indexOf(texto[i]) !=-1 ){ //indexOf devuelve -1 en caso de no encontrar elemento en la cadena
+          console.log(texto[i])
+          let posicion =((letras.indexOf(texto[i])+desplazamiento)%27); //necesitamos el módulo por si el resultado>26
+          console.log(posicion)
+          resultado +=letras[posicion]; //aquí se guardará esa letra nueva
+          console.log(resultado)
+        }
+        else
+        resultado +=texto[i]; //se muestra carácter igual al que se ingresó (simbolor, n°s, espacios, etc.)
+      }
+    } 
+    console.log(resultado)
+    return resultado;
+  },
+
+
+  decode: function (texto, desplazamiento) {
+  let resultado = "";
+  let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];   
+  desplazamiento = (desplazamiento % 27 - 27) % 27; //desplazamiento a la izquierda
+
+  if(texto){
+    for(let i=0; i<texto.length; i++){ 
+      console.log(texto)
+      if(letras.indexOf(texto[i]) !=-1 ){ 
+        console.log(texto[i])
+        let posicion =((letras.indexOf(texto[i])-desplazamiento)%27); 
+        console.log(posicion)
+        resultado +=letras[posicion]; 
+        console.log(resultado)
+      }
+      else
+      resultado +=texto[i]; 
+    }
+  } 
+  console.log(resultado)
+  return resultado;
+},
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js' ;
+
+window.addEventListener("load", inicio, true); 
+
+function inicio() {
+    document.getElementById("mensaje").addEventListener("keyup", function() {
+        this.value = this.value.toUpperCase(); //mensaje escuchado se cambia automáticamente a mayúsculas
+    }, true); 
+    
+    document.getElementById("cifrar").addEventListener("click", function(){
+        let texto = document.getElementById("mensaje").value; //click sobre btn cifrar nos devuelve un valor
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensajeSolicitado").value = cipher.encode(texto, desplazamiento);
+    }, true); //función devuelve un texto cifrado     
+
+    document.getElementById("descifrar").addEventListener("click", function(){
+        let texto = document.getElementById("mensaje").value; //click sobre btn cifrar nos devuelve un valor
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensajeSolicitado").value = cipher.decode(texto, desplazamiento);
+    }, true); //función devuelve texto descifrado
+}
 
 console.log(cipher);
